eaccf2257bce986a29e74e1f5d4f905cc7066402
Guilherme Reis <guilhermereis9876@gmail.com>
2020-11-19 22:08:42 -0300

    Footer

--- src/pages/Newsletter/index.js
+++ src/pages/Newsletter/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-import { Container, Main } from './styles';
+import { Container, Main } from "./styles";
 
-import Header from '../../components/Header';
-import Form from './Form';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Form from "./Form";
 
 function Newsletter() {
-  return <Container>
-    <Header/>
-    <Main>
-      <Form/>
-    </Main>
-  </Container>;
+  return (
+    <Container>
+      <Header />
+      <Main>
+        <Form />
+      </Main>
+      <Footer />
+    </Container>
+  );
 }
 
 export default Newsletter;
